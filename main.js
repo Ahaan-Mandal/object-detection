1430b8b93381ea8dbb22b2fc5e48f9601366d09c
Ahaan-Mandal <122046904+Ahaan-Mandal@users.noreply.github.com>
2023-06-12 18:00:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img= "";
 status = "";
+objects= [];
 
  function preload() {
     img= loadImage("dog_cat.jpg");
@@ -23,23 +24,31 @@ function modelLoaded() {
 function gotResult (error, results) {
 
     if (error){
-console.error("error")
+console.error("error");
     }
-    console.log(results)
+
+    console.log(results);
+    objects=results;
+
 }
 
 function draw() {
     image(img, 0, 0, 640, 420);
-    stroke("red");
-    fill("red");
-    text("Dog",35,60);
-    noFill();
-   rect(30,50, 400, 350);
+   
+    if (status != "")
+    {
+        for (i= 0; i < objects.length; i++)
+        {
 
-   stroke("red");
-   fill("red");
-   text("cat", 350,100);
-   noFill();
-   rect(300,85, 300, 300);
+            document.getElementById("status").innerHTML= "Status : Object Detected";
+
+            fill("red");
+            percent= floor(objects[i].confidence * 100);
+            text(objects[i].label + " " +  percent + "%" , objects[i].x, objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
     
 }
